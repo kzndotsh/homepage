951b6f292420b89a0753e2701255e724d6398a24
kaizen <admin@kaizen.wtf>
2023-09-02 02:51:44 -0400

    initial commit

---
+++ src/data/projects-data.js
@@ -0,0 +1,37 @@
+const projects = [
+  {
+    name: 'Planetaria',
+    description:
+      'Creating technology to empower civilians to explore space on their own terms.',
+    link: { href: 'http://planetaria.tech', label: 'planetaria.tech' },
+    logo: logoPlanetaria,
+  },
+  {
+    name: 'Animaginary',
+    description:
+      'High performance web animation library, hand-written in optimized WASM.',
+    link: { href: '#', label: 'github.com' },
+    logo: logoAnimaginary,
+  },
+  {
+    name: 'HelioStream',
+    description:
+      'Real-time video streaming library, optimized for interstellar transmission.',
+    link: { href: '#', label: 'github.com' },
+    logo: logoHelioStream,
+  },
+  {
+    name: 'cosmOS',
+    description:
+      'The operating system that powers our Planetaria space shuttles.',
+    link: { href: '#', label: 'github.com' },
+    logo: logoCosmos,
+  },
+  {
+    name: 'OpenShuttle',
+    description:
+      'The schematics for the first rocket I designed that successfully made it to orbit.',
+    link: { href: '#', label: 'github.com' },
+    logo: logoOpenShuttle,
+  },
+]
